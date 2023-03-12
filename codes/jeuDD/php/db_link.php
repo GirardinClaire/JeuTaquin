16784f57bf60dbd1ee98e63120aa6bb02dafff59
GIRARDIN Claire <116156608+GirardinClaire@users.noreply.github.com>
2023-03-12 21:51:39 +0100

    Update db_link.php

--- codes/jeuDD/php/db_link.php
+++ codes/jeuDD/php/db_link.php
@@ -2,7 +2,7 @@
 
 // PHP de connexion à la base de données AlwaysData avec le compte de Claire Girardin
 
-$link = pg_connect("host=postgresql-girardinclaire.alwaysdata.net dbname=girardinclaire_taquin_creation user=girardinclaire password=wT5RySfX#JE");
+$link = pg_connect("host=postgresql-girardinclaire.alwaysdata.net dbname=girardinclaire_taquin_creation user=girardinclaire password=***");
 
 if (!$link) {
     die('Erreur de connexion');
