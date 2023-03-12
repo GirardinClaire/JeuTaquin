a1cf54287636af5a69b0d9ec217f3a517ca85d5b
GIRARDIN Claire <116156608+GirardinClaire@users.noreply.github.com>
2023-03-12 21:53:33 +0100

    Update db_link.php

--- 2022-Taquin/EF49_Reveneau_TaquinCreation/Code/PHP/db_link.php
+++ 2022-Taquin/EF49_Reveneau_TaquinCreation/Code/PHP/db_link.php
@@ -1,5 +1,5 @@
 <?php
-$link = pg_connect("host=postgresql-hugoreveneau2.alwaysdata.net dbname=hugoreveneau2_taquin_creation user=hugoreveneau2 password=PYK+77!21n0xtt/]k,s7F");
+$link = pg_connect("host=postgresql-hugoreveneau2.alwaysdata.net dbname=hugoreveneau2_taquin_creation user=hugoreveneau2 password=***");
 
 // $link = pg_connect("host=localhost dbname=postgres user=postgres password=postgres");
 
